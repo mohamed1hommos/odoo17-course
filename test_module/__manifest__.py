82f0b108af878f59d47af1fa15c33ac07f70493c
Mohamed Atef <hommos181220@gmail.com>
2026-06-24 13:01:19 +0300

    Initial commit

---
+++ test_module/__manifest__.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'A_Test_Module_Mohamed',
+    'version': '17.0.1.0.0',
+    'category': 'Extra Tools',
+    'summary': 'Module to test odoo addons path',
+    'depends': ['base'],
+    'installable': True,
+    'auto_install': False,
+    'application': True,
+}
